54536db59bbb263212a4ca3afce85398601ba1e6
Isabella Benjey <bells@MacBook-Pro-22.local>
2022-12-08 17:29:56 -0500

    I'm Hoping

--- main.py
+++ main.py
@@ -14,14 +14,9 @@ conn = sqlite3.connect("anime.db")
 
 # Do each student's functions
 get_ghibli(conn)
+
 # ... call next student's function
 # ...
 
 # Call any other functions for calculations, graphs, etc.
-<<<<<<< HEAD
 # make_ghibli_graph()
-=======
-make_ghibli_graph()
-
-
->>>>>>> 1109d51518fe50965070824bc0c174c22ac34b61
